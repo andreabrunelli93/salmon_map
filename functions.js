7413abaa093bf784e594df2f22e9975078d4f88e
Andrea Brunelli <andrea.brunelli93@gmail.com>
2021-10-06 16:40:34 +0200

    text filter working

--- functions.js
+++ functions.js
@@ -9,4 +9,25 @@ var $btns = $('.btn').click(function() {
     }
     $btns.removeClass('active');
     $(this).addClass('active');
-  }) 
+  });
+
+
+  function textFilter() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("listings");
+    li = ul.getElementsByClassName('item');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
